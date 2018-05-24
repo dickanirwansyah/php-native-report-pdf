b837786ad641f33ce23e871486c48ab68ec59329
dickanirwansyah <dickanirwansyah@gmail.com>
2018-05-24 08:32:28 +0700

    format

--- index.php
+++ index.php
@@ -12,23 +12,23 @@
 	$pdf->Cell(190,7, 'Daftar Antrian',0,1, 'C');
 
 	// Memberikan space kebawah agar tidak terlalu rapat
-$pdf->Cell(10,7,'',0,1);
+	$pdf->Cell(10,7,'',0,1);
  
-$pdf->SetFont('Arial','B',10);
-$pdf->Cell(20,6,'kode',1,0);
-$pdf->Cell(85,6,'Nama',1,0);
-$pdf->Cell(27,6,'No Hp',1,0);
-$pdf->Cell(25,6,'Tanggal',1,1);
+	$pdf->SetFont('Arial','B',10);
+	$pdf->Cell(35,6,'kode',1,0);
+	$pdf->Cell(85,6,'Nama',1,0);
+	$pdf->Cell(30,6,'No Hp',1,0);
+	$pdf->Cell(30,6,'Tanggal',1,1);
  
-$pdf->SetFont('Arial','',10);
+	$pdf->SetFont('Arial','',10);
  
 include 'koneksi.php';
 $mahasiswa = mysqli_query($connect, "select * from antrian");
 while ($row = mysqli_fetch_array($mahasiswa)){
-    $pdf->Cell(20,6,$row['code_antrian'],1,0);
+    $pdf->Cell(35,6,$row['code_antrian'],1,0);
     $pdf->Cell(85,6,$row['nama'],1,0);
-    $pdf->Cell(27,6,$row['no_telepon'],1,0);
-    $pdf->Cell(25,6,$row['tanggal'],1,1); 
+    $pdf->Cell(30,6,$row['no_telepon'],1,0);
+    $pdf->Cell(30,6,$row['tanggal'],1,1); 
 }
  
 	$pdf->Output();
