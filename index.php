e4470e0946662f055f0f0271751d4d6543ddfe19
dickanirwansyah <dickanirwansyah@gmail.com>
2018-05-24 08:27:56 +0700

    fpdf library dan report pdf

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+	require('fpdf.php');
+
+	$pdf = new FPDF('l', 'mm', 'A5');
+
+	$pdf->AddPage();
+
+	$pdf->SetFont('Arial', 'B', 16);
+
+	$pdf->Cell(190,7, 'Laporan Antrian',0,1, 'C');
+	$pdf->SetFont('Arial', 'B', 12);
+	$pdf->Cell(190,7, 'Daftar Antrian',0,1, 'C');
+
+	// Memberikan space kebawah agar tidak terlalu rapat
+$pdf->Cell(10,7,'',0,1);
+ 
+$pdf->SetFont('Arial','B',10);
+$pdf->Cell(20,6,'kode',1,0);
+$pdf->Cell(85,6,'Nama',1,0);
+$pdf->Cell(27,6,'No Hp',1,0);
+$pdf->Cell(25,6,'Tanggal',1,1);
+ 
+$pdf->SetFont('Arial','',10);
+ 
+include 'koneksi.php';
+$mahasiswa = mysqli_query($connect, "select * from antrian");
+while ($row = mysqli_fetch_array($mahasiswa)){
+    $pdf->Cell(20,6,$row['code_antrian'],1,0);
+    $pdf->Cell(85,6,$row['nama'],1,0);
+    $pdf->Cell(27,6,$row['no_telepon'],1,0);
+    $pdf->Cell(25,6,$row['tanggal'],1,1); 
+}
+ 
+	$pdf->Output();
+?>
---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$password   = "root";
+$database   = "db_php";
+$connect    = mysqli_connect($host, $user, $password, $database);
+?>
